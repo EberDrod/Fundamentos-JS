17d1cb3f1e8210eb91569be774eed84ebc7cb70d
eberPeña <eberg.pena@gmail.com>
2019-01-10 17:36:32 -0600

    Se solicito parametros establecidos dentro del producto

---
+++ funcion.js
@@ -0,0 +1,14 @@
+var nombre = 'Eber'
+edad = 28
+
+
+
+
+//Definir una funcion (Parametros)
+function  imprimirEdad(n, e){
+    console.log (`${n} tiene ${e} años`)
+
+
+}
+
+imprimirEdad(nombre, edad)
