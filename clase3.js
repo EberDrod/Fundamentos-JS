4e18a1db4d8ed66e14d888a2953aafb789fed03b
eberPeña <eberg.pena@gmail.com>
2019-01-10 16:43:21 -0600

    Se agrego el modulo de JS Math.round para verificar que el numero se ha más preciso

--- clase3.js
+++ clase3.js
@@ -6,12 +6,24 @@ var edad = 27
 //Modo reducido
 edad += 1
 
-
+// Ejercicio de subir y bajar de peso
 var peso = 75
-
+//Decrementa todo lo que sale
 peso -= 2
 
 
 var sandwich = 1
+//HAce la suma de
+ peso += sandwich
 
-peso = peso + sandwich
+
+var jugarAlFutbol = 3
+
+peso -=   jugarAlFutbol
+
+
+var precioDeVino  = 200.3
+
+var total = precioDeVino * 100 *  3 / 100
+
+var total2 = Math.round(precioDeVino * 100 * 3) / 100
