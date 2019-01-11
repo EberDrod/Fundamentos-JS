afe3605b3f161b9fc99e409d1d02904beaa2cd48
eberPeña <eberg.pena@gmail.com>
2019-01-11 00:49:07 -0600

    Un objeto reune muchas caraacteristicas en comun

---
+++ clase6.js
@@ -0,0 +1,13 @@
+var nombreEber = 'Eber'
+var nombreDario = 'Dario'
+
+
+function imprimiNombeEnMayusculas(nombre){
+  nombre = nombre.toUpperCase()
+  console.log(nombre)
+
+
+}
+
+imprimiNombeEnMayusculas(nombreEber)
+imprimiNombeEnMayusculas(nombreDario)
