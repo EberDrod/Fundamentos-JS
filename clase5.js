025ed4cbfc0a820001fe9cbf08083d8539b5c084
eberPeña <eberg.pena@gmail.com>
2019-01-11 00:10:21 -0600

    Esta variable tiene un alcance de tipo localo

--- clase5.js
+++ clase5.js
@@ -1,8 +1,18 @@
 var nombre = 'Eber'
 
-function imprimirNombreEnMayusculas(){
-  nombre = nombre.toUpperCase()
-  console.log(nombre)
-}
+/// Estructura 1
+//function imprimirNombreEnMayusculas(){
+//  nombre = nombre.toUpperCase()
+  //console.log(nombre)
+//}
+/// Estructura 1
+//imprimirNombreEnMayusculas()
 
-imprimirNombreEnMayusculas()
+
+//Estructura 2
+function imprimirNombreEnMayusculas(n){
+  n = n.toUpperCase()
+  console.log(n)
+
+}
+imprimirNombreEnMayusculas(nombre)
