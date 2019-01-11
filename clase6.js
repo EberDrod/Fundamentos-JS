633b93ad5882277bf1dcf9ac73314a5c43a75b6c
eberPeña <eberg.pena@gmail.com>
2019-01-11 10:36:24 -0600

    Se configuro objeto agregando una segunda funcion para acceder al parametro establecido

--- clase6.js
+++ clase6.js
@@ -1,13 +1,39 @@
-var nombreEber = 'Eber'
-var nombreDario = 'Dario'
+//UN objeto reune caracteristicas que tienen en comun pueden ser strings nuemeroetc
 
-
-function imprimiNombeEnMayusculas(nombre){
-  nombre = nombre.toUpperCase()
-  console.log(nombre)
+var eber = {
+  //CLAVE & VALOR
+  nombre: 'Eber',
+  apellido: 'Peña',
+  edad:28
 
 
 }
 
-imprimiNombeEnMayusculas(nombreEber)
-imprimiNombeEnMayusculas(nombreDario)
+var dario = {
+
+  nombre:'dario',
+  apellido: 'Sarduscky' ,
+  edad:27
+
+
+}
+
+//var nombreDario =
+
+function imprimiNombeEnMayusculas({nombre}){
+    //var nombre = persona.nombre.toUpperCase()
+    console.log(nombre.toUpperCase())
+}
+
+function imprimiNombeEnMayusculas2({apellido}){
+      console.log(apellido.toUpperCase())
+
+
+}
+
+
+
+imprimiNombeEnMayusculas(eber)
+imprimiNombeEnMayusculas(dario)
+imprimiNombeEnMayusculas({nombre: 'Pepito' })
+imprimiNombeEnMayusculas2({apellido: 'Gomez' })
